e465b82d74045c1df55bc010a1ed31406229316b
shani-tiwari <shanitiwari2021@gmail.com>
2026-01-29 17:09:54 +0530

    pwa add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,9 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import AppRoutes from "./routes/AppRoutes";
-import { getProjects } from "./api/project.api";
-import { useState, useEffect } from "react";
+
 
 export default function App() {
-  const [result, setresult] = useState([]);
-
-  useEffect(() => {
-    // console.log(getProject.then(res => res.data.Projects))
-    getProjects().then((res) => setresult(res.data.Projects));
-
-    console.log(result);
-  });
 
   return (
     <>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,84 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     port: 5173,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     strictPort: true,
     tunnel: false,
     open: false,
   },
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      manifest: {
+        name: "BeBD",
+        short_name: "BeBD",
+        description: "BeBD",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "icons/icons/icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: "icons/icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "icons/icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "icons/icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "icons/icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "icons/icons/icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+          {
+            src: "icons/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "icons/icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "icons/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+      },
+    }),
+  ],
+});
