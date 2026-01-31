eac8183e982a0c591290d80edbd7f14ef0d49027
shani-tiwari <shanitiwari2021@gmail.com>
2026-01-31 09:24:56 +0530

    production ready

--- backend/src/controller/image.controller.js
+++ backend/src/controller/image.controller.js
@@ -3,10 +3,10 @@ const imagekit = require("../services/imagekit");
 const imagekitAuth = (req, res) => {
   try {
     const authParams = imagekit.getAuthenticationParameters();
-    console.log("Auth Params:", JSON.stringify(authParams, null, 2));
+    // console.log("Auth Params:", JSON.stringify(authParams, null, 2));
     res.json(authParams);
   } catch (error) {
-    console.error("Error:", error.message);
+    // console.error("Error:", error.message);
     res.status(500).json({ error: error.message });
   }
 };
