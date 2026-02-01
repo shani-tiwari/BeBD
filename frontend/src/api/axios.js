b561e44bbdd11f32195b10ac7c8481b10036beef
shani-tiwari <shanitiwari2021@gmail.com>
2026-02-01 20:16:43 +0530

    frontend setup

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
- const api = axios.create({
-    baseURL: 'https://bebd.vercel.app/api/v1',
-    withCredentials: true
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "https://bebd.vercel.app/api/v1",
+  withCredentials: true,
 });
 
 export default api;
--- frontend/src/api/imagekit.api.js
+++ frontend/src/api/imagekit.api.js
@@ -1,26 +1,26 @@
-import Imagekit from "@imagekit/javascript"
+import Imagekit from "@imagekit/javascript";
 import api from "./axios";
 
-export const uploadImage = async(file) => {
-    try {
-        const {data} = await api.get("/auth/imagekit")
-        
-        const ik = new Imagekit({
-            publicKey: import.meta.env.VITE_IMAGEKIT_PUBLIC_KEY,
-            urlEndpoint: import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT,
-            authenticationEndpoint: import.meta.env.VITE_IMAGEKIT_AUTHENTICATION_ENDPOINT,
-            
-        });
+export const uploadImage = async (file) => {
+  try {
+    const { data } = await api.get("/auth/imagekit");
 
-        return ik.upload({
-            file,
-            fileName: file.name,
-            token: data.token,
-            signature: data.signature,
-            expire: data.expire
-        });
-    } catch (error) {
-        console.log(error);
-    }
+    const API_URL =
+      import.meta.env.VITE_API_URL || "https://bebd.vercel.app/api/v1";
+    const ik = new Imagekit({
+      publicKey: import.meta.env.VITE_IMAGEKIT_PUBLIC_KEY,
+      urlEndpoint: import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT,
+      authenticationEndpoint: `${API_URL}/auth/imagekit`,
+    });
 
-}
+    return ik.upload({
+      file,
+      fileName: file.name,
+      token: data.token,
+      signature: data.signature,
+      expire: data.expire,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
