0c2f82ca0c3824d5e62d0527b1f060e00e623b5f
shani-tiwari <shanitiwari2021@gmail.com>
2026-02-02 07:19:50 +0530

    bug fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,9 +15,8 @@ googleAuth();
 app.use(
   cors({
     origin: [
-      "https://bebd.vercel.app",
+      "https://bebd-be-better-developer.vercel.app/",
       "https://be-bd-be-better-developer-8cf31fgzz.vercel.app",
-      "https://bebd-be-better-developer.vercel.app/"
     ],
     credentials: true,
   }),
